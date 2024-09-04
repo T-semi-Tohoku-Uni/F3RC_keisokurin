cca3ccfde0a33f97995b410809278b288c1ec209
grafen <hiroyawatanabe0519@gmail.com>
2024-09-05 00:11:50 +0900

    Z相追加

--- Core/Inc/stm32g4xx_it.h
+++ Core/Inc/stm32g4xx_it.h
@@ -59,6 +59,7 @@ void FDCAN1_IT0_IRQHandler(void);
 void FDCAN1_IT1_IRQHandler(void);
 void TIM4_IRQHandler(void);
 void USART2_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
 void TIM7_DAC_IRQHandler(void);
 /* USER CODE BEGIN EFP */
